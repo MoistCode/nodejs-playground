442f9f9e71d7800d9cbeb50d65df8d9a4535e763
Tommy Pham <phamtommylong1991@gmail.com>
2019-06-30 17:41:44 -0700

    node did nothing

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 
 // This function will run for every request sent to the server
-function requestListener(req, res) {}
+function requestListener(req, res) {
+  console.log(req);
+}
 
 http.createServer(requestListener);
