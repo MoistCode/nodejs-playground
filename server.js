45f278d7ea16cf385fe509f294ee9149b48564b6
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-02 18:00:59 -0700

    set header

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require("http");
 
 // This function will run for every request sent to the server
 function requestListener(req, res) {
-  console.log(req.url, req.method, req.headesr);
+  console.log(req.url, req.method, req.headers);
   // process.exit();
+  res.setHeader("Content-Type", "text/html");
 }
 
 const server = http.createServer(requestListener);
