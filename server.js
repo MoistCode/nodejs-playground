426c2df02edc45d122cf47f50a25b827d776c866
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-02 17:48:47 -0700

    added exit code

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const http = require("http");
 // This function will run for every request sent to the server
 function requestListener(req, res) {
   console.log(req);
+  // process.exit();
 }
 
 const server = http.createServer(requestListener);
-const PORT = 3000;
+const PORT = 4000;
 
 server.listen(PORT);
