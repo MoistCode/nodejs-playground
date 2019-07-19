c20e481773d2415614186f0bf9996e8f4a93c8e6
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-19 11:19:18 -0700

    filesystel stuff

--- server.js
+++ server.js
@@ -5,39 +5,46 @@
  */
 
 const http = require("http");
+const fs = require("fs");
 
 function requestListener(req, res) {
-    // This is what is being sent to us
-    const { url, method, headers } = req;
+  // This is what is being sent to us
+  const { url, method, headers } = req;
 
-    console.log(url, method, headers);
+  console.log(url, method, headers);
 
-    // process.exit();
+  // process.exit();
 
-    switch (url) {
-        default:
-            res.write(`
-          <html>
-            <head>
-              <title>Homepage</title>
-            </head>
-            <body>
-              <h1>This is the homepage</h1>
-              <form action="/message" method="POST">
-                <input type="text">
-                <button type="submit">
-                  Submit
-                </button>
-              </form>
-            </body>
-          </html>
-        `);
-            break;
-    }
+  switch (url) {
+    case "/message":
+      if (method === "POST") {
+      }
 
-    res.setHeader("Content-Type", "text/html");
+      break;
+    default:
+      res.write(`
+        <html>
+          <head>
+            <title>Homepage</title>
+          </head>
+          <body>
+            <h1>This is the homepage</h1>
+            <form action="/message" method="POST">
+              <input type="text" name="message">
+              <button type="submit">
+                Submit
+              </button>
+            </form>
+          </body>
+        </html>
+      `);
 
-    res.end();
+      break;
+  }
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.end();
 }
 
 const server = http.createServer(requestListener);
