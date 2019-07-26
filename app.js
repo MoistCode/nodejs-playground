f5a6af43675eeb49524cfe96d3c44b0db590ca83
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-26 14:16:24 -0700

    added some routes

--- app.js
+++ app.js
@@ -16,20 +16,23 @@ const app = express();
 
 const PORT = 3000;
 
-app.use((req, res, next) => {
-  console.log("middleware1");
-  next(); // Necessary to move forward
+app.use("/", (req, res, next) => {
+  console.log("middleware");
+
+  next();
 });
 
-app.use((req, res, next) => {
-  console.log("middleware2");
-  next(); // Necessary to move forward
+app.use("/add-product", (req, res, next) => {
+  res.send(`
+    <h1>Add Product</h1>
+  `);
 });
 
-app.use((req, res, next) => {
-  console.log("middleware3");
-
-  res.send();
+// Gets executed for / and /add-product
+app.use("/", (req, res, next) => {
+  res.send(`
+    <h1>Homepage</h1>
+  `);
 });
 
 // const server = http.createServer(app);
