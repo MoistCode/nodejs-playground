3ee79ff551f70f9314b2b4c083e91bdd88714954
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-27 14:28:44 -0700

    added correct request method

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
   res.send(`
@@ -29,14 +29,14 @@ app.use("/add-product", (req, res, next) => {
   `);
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   const { body } = req;
 
   res.redirect("/");
 });
 
 // Gets executed for / and /add-product
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.send(`
     <h1>Homepage</h1>
   `);
