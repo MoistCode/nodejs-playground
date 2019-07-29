006f8799342e3a044d5d71af02800926d583c971
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-29 15:36:33 -0700

    handle 404

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRouter);
 app.use(shopRouter);
 
+// Handles 404
+app.use((req, res, next) => {
+  res.status(404).send(`
+    <h1>Page not found</h1>
+  `);
+});
+
 // const server = http.createServer(app);
 
 // server.listen(PORT);
