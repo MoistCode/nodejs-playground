9d41c7a7de79e9bc17715c40dd47c5fb689de7dc
Tommy Pham <phamtommylong1991@gmail.com>
2019-07-30 15:52:29 -0700

    broke out html

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 // Gets executed for / and /add-product
 router.use("/", (req, res, next) => {
-  res.sendFile("/views/shop.html");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = { shopRouter: router };
