b26ade0cebc4a43eada88a7fa73df620230c6099
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-08 18:54:52 -0700

    added render

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,7 @@
 exports.add = (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
+  });
 };
