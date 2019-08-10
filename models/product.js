29667b074468a9f43cdb89d322b4f3871e956e34
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-09 19:07:57 -0700

    added save and fetch

--- models/product.js
+++ models/product.js
@@ -1,5 +1,16 @@
+const products = [];
+
 module.exports = class Product {
+  static fetchAll() {
+    return products;
+  }
+
   constructor({ title }) {
     this.title = title;
+  }
+
+  save() {
+    products.push(this);
+    return this;
   }
 };
