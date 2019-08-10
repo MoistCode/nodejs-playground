30e4c038f5ed39553146ae572389d4f277100fc2
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-09 19:09:51 -0700

    added some static and class methods

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,9 @@
+const Product = require("../models/product");
+
 exports.getAddProducts = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  const product = Product.fetchAll();
+
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product"
@@ -8,6 +12,9 @@ exports.getAddProducts = (req, res, next) => {
 
 exports.postAddProducts = (req, res, next) => {
   const { body } = req;
+
+  const product = new Product(req.body.title);
+  Product.save();
 
   res.redirect("/");
 };
