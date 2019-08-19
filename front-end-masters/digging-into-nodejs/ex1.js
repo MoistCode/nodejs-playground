43eac46441cd86ac9c6aa2661904c637787684ad
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-19 13:59:05 -0700

    nodejs notes

--- front-end-masters/digging-into-nodejs/ex1.js
+++ front-end-masters/digging-into-nodejs/ex1.js
@@ -1,6 +1,7 @@
 /**
  *  How does Node connects the its environment
  *  POSIX: C style programs integrate with linux style operating systems
+ *  Standard I/O stdin stdout stderr
  */
 
 console.log("Hello world!");
