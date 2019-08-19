37a8bfbcd9b1f7136d620fe987b4bff5bfe8516d
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-19 14:01:53 -0700

    nodejs notes

--- front-end-masters/digging-into-nodejs/ex1.js
+++ front-end-masters/digging-into-nodejs/ex1.js
@@ -2,6 +2,9 @@
  *  How does Node connects the its environment
  *  POSIX: C style programs integrate with linux style operating systems
  *  Standard I/O stdin stdout stderr
+ *  Javascript is agnostic to IO
+ *  JS is easy to adapt because it doesn't make assumption about communication; env sets that
  */
 
-console.log("Hello world!");
+process; // Object available to all Node programs; connect to environment
+process.stdout.write("Hello"); // Streams are first-class citizens
