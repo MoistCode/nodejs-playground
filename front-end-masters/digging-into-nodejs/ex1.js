f978b7319159beb7a97ffff332fbf670564d2944
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-19 14:06:53 -0700

    nodejs notes

--- front-end-masters/digging-into-nodejs/ex1.js
+++ front-end-masters/digging-into-nodejs/ex1.js
@@ -7,4 +7,13 @@
  */
 
 process; // Object available to all Node programs; connect to environment
-process.stdout.write("Hello"); // Streams are first-class citizens
+process.stdout.write("Hello"); // Streams are first-class citizens; least efficient way to write a string
+console.log("Hello"); // Send binary stream with buffer to become more effective
+
+/**
+ * process.stdout.write will not provide a trailing newline
+ * console.log does a lot more than just calling process.std.write
+ */
+
+process.stderr.write("Some error");
+console.error("Some error");
